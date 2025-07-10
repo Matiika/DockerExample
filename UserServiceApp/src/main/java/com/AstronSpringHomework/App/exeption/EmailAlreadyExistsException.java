be43dfc1ecea4f1d688a43ebc5efaae08ca80938
Matvei Trukhan <45766685+Matiika@users.noreply.github.com>
2025-07-10 22:58:01 +0300

    Delete UserServiceApp directory

--- UserServiceApp/src/main/java/com/AstronSpringHomework/App/exeption/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.AstronSpringHomework.App.exeption;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- UserServiceApp/src/main/java/com/AstronSpringHomework/App/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.AstronSpringHomework.App.exeption;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
